677e59c27d27102b54dd83c265b41db4cdad4c27
Nailton Sousa <nailton.osn@gmail.com>
2018-12-14 18:07:40 -0200

    Update php7.3 in others scripts

--- scripts/zeromq.sh
+++ scripts/zeromq.sh
@@ -8,13 +8,13 @@ PHP_IS_INSTALLED=$?
 
 echo ">>> Installing ØMQ"
 
-sudo add-apt-repository -qq pp:chris-lea/zeromq 
+sudo add-apt-repository -qq pp:chris-lea/zeromq
 sudo apt-get update -qq
-sudo apt-get install -qq libtool autoconf automake uuid uuid-dev uuid-runtime build-essential php7.0-dev pkg-config libzmq3-dbg libzmq3-dev libzmq3
+sudo apt-get install -qq libtool autoconf automake uuid uuid-dev uuid-runtime build-essential php7.3-dev pkg-config libzmq3-dbg libzmq3-dev libzmq3
 
 echo "" | sudo pecl install zmq-beta > /dev/null
 
-sudo echo "extension=zmq.so" >> /etc/php/7.0/mods-available/zmq.ini
+sudo echo "extension=zmq.so" >> /etc/php/7.3/mods-available/zmq.ini
 sudo phpenmod zmq > /dev/null
-sudo service php7.0-fpm restart > /dev/null
+sudo service php7.3-fpm restart > /dev/null
 
